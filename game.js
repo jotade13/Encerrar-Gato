54835abed028b5169f6a6610a46415b602b73b6b
jotade13 <jdmds13@gmail.com>
2024-01-10 15:16:27 -0400

    creacion de las clases, Juego, Casilla y Gato

--- game.js
+++ game.js
@@ -1,8 +1,36 @@
-class juego{
-    33
+class Juego{
+    constructor()
+    {
+        let casilla = [];
+        for(let i=0;i<10;i++)
+        {
+            casilla[i] = [];
+            for(let j=0;j<10;j++)
+            {
+                casilla[i][j] = new Casilla(i,j);
+                console.log(casilla[i][j]);
+            }
+        }
+        let gato = new Gato();
+    }
 }
-class casilla{
-    estado = false;
-    alrededor = [];
-    gato = false;
+class Casilla{
+    constructor(fila,col){
+        this._fila= fila;
+        this._col= col;
+        this._estado = false;
+        this._gato = false;
+    }
+    
+}
+class Gato{
+    constructor()
+    {
+        this._fila = 5;
+        this._col = 5;
+    }
+}
+window.onload= function()
+{
+    juego = new Juego();
 }
